8c3dfdd58328a252005dbced0ea9cab27856cdd0
dankwon234 <dan.kwon234@gmail.com>
2016-04-06 11:42:19 -0400

    tablview datasource methods

--- 3-celebrities/celebrities/DKChatViewController.swift
+++ 3-celebrities/celebrities/DKChatViewController.swift
@@ -8,22 +8,51 @@
 
 import UIKit
 
-class DKChatViewController: UIViewController {
+class DKChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var chatTable: UITableView!
     
     override func loadView() {
         let frame = UIScreen.mainScreen().bounds
         let view = UIView(frame: frame)
         view.backgroundColor = UIColor.redColor()
         
+        self.chatTable = UITableView(frame: frame, style: .Plain)
+        self.chatTable.dataSource = self
+        self.chatTable.delegate = self
+        view.addSubview(self.chatTable)
+        
         self.view = view
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    
+    // MARK: - DataSource Methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20 // allocate 20 rows for the table view
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellId = "cellId"
+        if let cell = tableView.dequeueReusableCellWithIdentifier(cellId) {
+            cell.textLabel?.text = "\(indexPath.row)"
+            return cell
+        }
 
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: cellId)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
